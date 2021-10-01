c5bd50225552bbf7184d14e52b5ae649cc812dd4
ADARSH N <adarshkumar738291@gmail.com>
2021-10-01 17:53:51 +0530

    Add files via upload

---
+++ random.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class random {
+        public static void main(String[] args)
+        {
+            Scanner cs = new Scanner(System.in);
+            System.out.print("Enter the number of digits to generate random :-");
+            int numb = cs.nextInt();
+            System.out.println(numb);
+            cs.close();
+        }
+}
